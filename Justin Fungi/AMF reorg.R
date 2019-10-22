85bca9854564aaab2e9b31fcd19734be13ef359e
Justin Day <jday9@uoregon.edu>
2019-10-22 15:48:10 -0700

    added colors to AMF and richness scatterplot

--- Justin Fungi/AMF reorg.R
+++ Justin Fungi/AMF reorg.R
@@ -255,15 +255,19 @@ ggplot(subset(plant4,fungi=="amf"), aes(y=mean,x=nfixer))+
   theme(legend.position="none", axis.text=element_text(size=16), axis.title=element_text(size=16), plot.title = element_text(size = 18, face = "bold"), strip.text.x = element_text(size = 16))
 
 #richness*amf
-ggplot(subset(plant4,fungi=="amf"), aes(y=richness,x=mean))+
+ggplot(subset(plant4,fungi=="amf"), aes(y=richness,x=mean, color=ppt_trt))+
   geom_point()+ 
   geom_smooth(method="lm", se=F)+ 
   facet_wrap(~ppt_trt)+
   xlab("AMF colonization")+ 
   ylab("richness")+ 
-  ggtitle("AMF vs. richness")+
-  theme_classic() + 
-  theme(legend.position="none", axis.text=element_text(size=16), axis.title=element_text(size=16), plot.title = element_text(size = 18, face = "bold"), strip.text.x = element_text(size = 16))
+  ggtitle("Regression of Plot Richness with AMF Colonization Across Precipitation Treatments")+
+  theme_classic()+
+  theme(legend.position="none", axis.text=element_text(size=16), axis.title=element_text(size=16), plot.title = element_text(size = 18, face = "bold"), strip.text.x = element_text(size = 16))+
+  scale_color_manual(values = c("indianred1","skyblue2","lightgoldenrod2" ), 
+                     guide = guide_legend(title = "Precipitation Treatment"),
+                     labels=c("Drought", "Ambient", "High"))
+
 
 #diversity*nutrients
 ggplot(plant4,aes(x=nut_trt, y=diversity))+
@@ -273,9 +277,7 @@ ggplot(plant4,aes(x=nut_trt, y=diversity))+
   ggtitle("")+
   scale_x_discrete(labels=c("Compost", "Fertilizer","No Amendment")) +
   theme(legend.position=c(0.8,0.8), legend.title=element_text(size=14), legend.text=element_text(size=12), axis.text=element_text(size=16), axis.title=element_text(size=16), plot.title = element_text(size = 18, face = "bold"))+
-  scale_fill_manual(values = c("indianred1",  "lightgoldenrod2", "skyblue2"), #changes colors of points (use scale_fill_manual for boxplots and bar plots)
-                    guide = guide_legend(title = "Precipitation Treatment"), #change legend title
-                    labels=c("Drought", "Ambient", "High")) #change labels in the legend
+
 
 #evenness*amf
 ggplot(subset(plant4,fungi=="amf"), aes(y=evenness,x=mean))+
