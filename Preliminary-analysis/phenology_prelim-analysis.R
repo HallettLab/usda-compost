720cfc3bcad3cb5676d00c7879cdb4f8a3266a06
Caitlin White <caitlin.t.white@colorado.edu>
2020-01-11 10:12:34 -0700

    minor edits to AS code so temporal lines plot, add datpath code at top

--- Preliminary-analysis/phenology_prelim-analysis.R
+++ Preliminary-analysis/phenology_prelim-analysis.R
@@ -1,17 +1,28 @@
-setwd("~/Dropbox/USDA-compost/Data")
-pheno<-read.csv("Phenology/Phenology_CleanedData/Compost_Phenology_Clean.csv")
+#setwd("~/Dropbox/USDA-compost/Data")
+# specify dropbox pathway (varies by user -- ctw can tweak this later)
+if(file.exists("~/DropboxCU/Dropbox/USDA-compost/Data/")){
+  ## CTW
+  datpath <- "~/DropboxCU/Dropbox/USDA-compost/Data/"
+}else{
+  ## LMH and AS
+  datpath <- "~/Dropbox/USDA-compost/Data/"
+}
+
+pheno<-read.csv(paste0(datpath, "Phenology/Phenology_CleanedData/Compost_Phenology_Clean.csv"))
 
 #show phenology overtime by nut_trt, ppt_trt and elevation
 pheno_gf<-pheno %>% filter(plot!="NA")%>%
   mutate(site = "high", 
-         site = ifelse(block == 1 | block == 2, "low", site)) %>%
+         site = ifelse(block == 1 | block == 2, "low", site),
+         date = as.Date(date, format = "%Y-%m-%d")) %>%
   mutate(ppt_trt=ordered(ppt_trt, levels = c(D="D", XC="XC", W="W"))) %>%
   group_by(date, site, nut_trt, ppt_trt) %>%
-  summarize(meanPG=mean(pct_green), sePG=sd(pct_green)/sqrt(length(pct_green)))
+  summarize(meanPG=mean(pct_green), sePG=sd(pct_green)/sqrt(length(pct_green))) %>%
+  ungroup()
 
 ggplot(data=pheno_gf, aes(x=date, y=meanPG, color=nut_trt))+
   facet_wrap(~site*ppt_trt)+
-  geom_point(aes(cex=1.5))+
+  geom_point(cex=1.5)+
   geom_errorbar(aes(ymax = meanPG+sePG, ymin = meanPG-sePG), width=.25)+
   geom_line()+
   labs(x="Date", y="Percent Green") +
@@ -22,14 +33,16 @@ ggplot(data=pheno_gf, aes(x=date, y=meanPG, color=nut_trt))+
 #pheno plot for low dry plots only
 pheno_gf2<-pheno %>% filter(plot!="NA")%>%
   mutate(site = "high", 
-         site = ifelse(block == 1 | block == 2, "low", site)) %>%
+         site = ifelse(block == 1 | block == 2, "low", site),
+         date = as.Date(date, format = "%Y-%m-%d")) %>%
   filter(site=="low")%>%
   filter(ppt_trt=="D")%>%
   group_by(date, nut_trt) %>%
-  summarize(meanPG=mean(pct_green), sePG=sd(pct_green)/sqrt(length(pct_green)))
+  summarize(meanPG=mean(pct_green), sePG=sd(pct_green)/sqrt(length(pct_green))) %>%
+  ungroup()
 
 ggplot(data=pheno_gf2, aes(x=date, y=meanPG, color=nut_trt))+
-  geom_point(aes(cex=1.5))+
+  geom_point(cex=1.5)+
   geom_errorbar(aes(ymax = meanPG+sePG, ymin = meanPG-sePG), width=.25)+
   geom_line()+
   labs(x="Date", y="Percent Green") +
