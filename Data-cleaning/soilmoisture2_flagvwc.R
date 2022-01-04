6e67279dd403bd2449ea6c179b3705bf609c66d2
Caitlin White <caitlin.t.white@colorado.edu>
2022-01-03 23:03:20 -0800

    add range and streak flag functions based on lit

--- Data-cleaning/soilmoisture2_flagvwc.R
+++ Data-cleaning/soilmoisture2_flagvwc.R
@@ -145,13 +145,120 @@ ggsave(filename = paste0(datpath, "SoilMoisture/SoilMoisture_DataQA/PrelimQA_Fig
        width = 10, height = 6, units = "in")
 
 
+group_by(smdat, portid) %>%
+  subset(is.na(qa_note)) %>%
+  summarise(nobs = length(vwc)) %>%
+  arrange(nobs) %>%
+  data.frame()
+
+# -- TEST CASE -----
+# apply QA/QC methods from lit
+# subset dat to a relatively clean/trustworthy line
+# > who has the fewest qa notes?
+group_by(smdat, portid) %>%
+  mutate(totobs = length(vwc)) %>%
+  subset(is.na(qa_note)) %>%
+  group_by(portid, fulltrt, totobs) %>%
+  summarise(prop.clean = (length(vwc) / totobs)) %>%
+  distinct() %>%
+  arrange(totobs, prop.clean) %>%
+  data.frame()
+# b2l1 = non-irrigated test case, b3l1 = irrigated fert and comp, and one fert drought
+testcase <- subset(smdat, logger == "B2L1")
+rownames(testcase) <- NULL  # clear rownames
+testcase$rowid <- as.numeric(rownames(testcase))
+  
+  mutate(rowid = as.numeric(rownames(.)))
+
+# range check
+check_range <- function(dat, id = c("rowid", "portid", "clean_datetime", "cleanorder"), hi = 0.6, low = -0.04){
+  tempdat <- subset(dat, select = c(id, "vwc"))
+  tempdat$flag_range[tempdat$vwc > hi] <- "high warning"
+  tempdat$flag_range[tempdat$vwc <= low] <- "outside low"
+  tempdat$flag_range[tempdat$vwc < 0 & tempdat$vwc > low] <- "low warning"
+  return(tempdat)
+}
+
+# apply
+rangetest <- check_range(testcase)
+# plot
+ggplot(rangetest, aes(clean_datetime, vwc, group = portid)) +
+  geom_line() +
+  geom_point(data=subset(rangetest, !is.na(flag_range)), aes(clean_datetime, vwc, col = flag_range)) +
+  facet_wrap(~portid)
+
+
+# flatline of 10+ timesteps (don't think there are any but to be sure)
+check_streak <- function(dat, id = c("rowid", "clean_datetime", "cleanorder"), groupvars = c("portid"), steps = 10){
+  tempdat <- subset(dat, select = c(id, groupvars, "vwc")) %>%
+    grouped_df(groupvars) %>%
+    mutate(diff = vwc == lag(vwc))
+  streakruns <- list2DF(rle(tempdat$diff))
+  # calculate startpos for streakruns
+  streakruns$endpos <- cumsum(streakruns$lengths)
+  streakruns$startpos <- with(streakruns, endpos - lengths)
+  # subset to streakruns that meet or exceed flatline threshold
+  streakruns <- subset(streakruns, lengths >= steps & values)
+  # create flag col
+  tempdat$flag_streak <- NA
+  tempdat$streak_num <- NA
+  for(i in 1:nrow(streakruns)){
+    tempdat$flag_streak[streakruns$startpos[i]:streakruns$endpos[i]] <- paste0("flatline (", streakruns$lengths[i]+1, " timesteps)")
+    tempdat$streak_num[streakruns$startpos[i]:streakruns$endpos[i]] <- i
+  }
+  # clean up
+  tempdat <- ungroup(subset(tempdat, select = -diff))
+  return(tempdat)
+}
+
+# apply
+streaktest <- check_streak(testcase)
+# plot
+ggplot(subset(streaktest, portid %in% portid[!is.na(flag_streak)] & cleanorder %in% cleanorder[!is.na(flag_streak)]),aes(clean_datetime, vwc, group = portid)) +
+  geom_line() +
+  geom_point(data=subset(streaktest, !is.na(flag_streak)), aes(clean_datetime, vwc, col = flag_streak)) +
+  facet_wrap(~streak_num, scales = "free")
+
+
+# target day exceeds +3 z-score in 30d window
+check_deviance <- function(dat, id = c("rowid", "clean_datetime", "cleanorder"), groupvars = c("portid"), rollwin = (30)){
+  tempdat <- subset(dat, portid == "B2L1_1", select = c(id, groupvars, "vwc")) %>%
+    grouped_df(groupvars)
+  tempdat$rolling <- rollapply(tempdat$vwc, rollwin, fill = NA, function(x) scale(x))
+}
+
+ggplot(tempdat, aes(clean_datetime, vwc)) +
+  geom_line() +
+  geom_point(data = subset(tempdat, abs(rolling) >=3),aes(clean_datetime, vwc), col = "orchid", size = 2)
+
+
+# target day exceeds +3 z-score in same-day over all years step change
+check_step <- function(dat, id = c("rowid", "clean_datetime", "date", "cleanorder"), groupvars = c("portid"), rollwin = (30)){
+  tempdat <- subset(dat, portid == "B2L1_1", select = c(id, groupvars, "vwc")) %>%
+    grouped_df(groupvars)
+  tempdat$rolling <- rollapply(tempdat$vwc, rollwin, fill = NA, function(x) scale(x))
+}
+
+# all 3 criteria must fail to flag
+check_spike <- function(){
+  # 1. subsequent time step change of 15%
+  
+  # 2. second deriv outside 0.8 to 1.2 (based on calibrated data in doriga et al. 2013)
+  
+  # 3. 24hr CV exceeds 1
+  
+}
+
+# wetup event and no observed rainfall in the previous 24 hrs
+# > either get irrigation schedule for W plots or only run this on XC and D ppt trts (W in summer okay)
+outside_precip
 
 # -- LOGIC CHECKS -----
 # flag 1 = value outside of plausible range? ([0-])
 # > allow for slight dip below 0 that can be shifted to 0 or up (manual says accurate to 0.03 m3/m3 [3% VWC])
 smdat_qa <- subset(smdat, select = c(logger:vwc)) %>%
   mutate(rowid = rownames(.),
-         range_flag = ifelse(vwc > 0.7, "high warning",
+         range_flag = ifelse(vwc > 0.6, "high warning",
                              ifelse(vwc < 0 & vwc > -0.04, "low warning", 
                                     ifelse(vwc <= -0.04, "outside range", NA))),
          # start qa_vwc
@@ -166,7 +273,7 @@ rundf <- data.frame()
 for(p in unique(smdat$portid)){
   tempdat <- subset(smdat_qa, portid == p) %>%
     mutate(diffvwc = round(vwc - lag(vwc),2),
-           wetup = diffvwc >= 0.2 | (diffvwc > 0.01 & lead(diffvwc) > 0.15) | (diffvwc >= 0.01 & lag(diffvwc) > 0.15),
+           wetup = diffvwc >= 0.15 | (diffvwc > 0.01 & lead(diffvwc) > 0.15) | (diffvwc >= 0.01 & lag(diffvwc) > 0.15),
            wetup_event = as.numeric(NA))
   temprun <- list2DF(rle(tempdat$wetup))
   
@@ -203,7 +310,7 @@ accumulated <- group_by(rundf, portid, waterYear) %>%
   mutate(flagwetup = !is.na(wetup_event) & is.na(rainevent)) %>%
   # try more conservative wetupflag
   group_by(date, portid) %>%
-  mutate(dryday = sum(ppt_mm, na.rm = T) < 0.01) %>%
+  mutate(dryday = sum(ppt_mm, na.rm = T) < 0.02) %>%
   ungroup() %>%
   mutate(flagwetup2 = !is.na(wetup_event) & dryday)
 
@@ -214,7 +321,7 @@ ggplot(accumulated, aes(wY_accumulated_ppt, accumulated_sm)) +
 
 # plot soil moisture with flagged values in red (this is not yet taking into account irrigation for wet plots)
 ggplot() +
-  geom_line(data = accumulated, aes(clean_datetime, qa_vwc, group = portid)) +
+  geom_line(data = subset(accumulated, !flagwetup2), aes(clean_datetime, qa_vwc, group = portid)) +
   geom_point(data = subset(accumulated, flagwetup), aes(clean_datetime, vwc), col = "red", alpha = 0.5) +
   geom_point(data = subset(accumulated, flagwetup2), aes(clean_datetime, vwc), col = "yellow", alpha = 0.5) +
   facet_grid(nut_trt~ppt_trt)
