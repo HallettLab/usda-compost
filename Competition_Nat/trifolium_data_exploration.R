abe9857c46c14d94521008781c0e12a7783508d0
ebel-c <cebel2@uoregon.edu>
2022-02-02 18:19:34 -0800

    CE adding trifol data exploration script for NK

---
+++ Competition_Nat/trifolium_data_exploration.R
@@ -0,0 +1,61 @@
+## Initial data exploration - trifolium
+
+## read in data -> this part will need to be different depending on where you store your data.
+dat <- read.csv("competition_trifolium_seeds_2021.csv")
+
+## load packages
+library(tidyverse)
+
+## divide total seeds by total stems to standardize seed output
+dat <- dat %>%
+  mutate(seeds_per_stem = tot_seeds/tot_stems)
+
+
+## VISUALLY EXPLORE DATA! 
+## want to visualize seed output by precipitation condition
+## how does this differ by competition background?
+
+## seeds per stem vs. precipitation treatment; faceted by competition background
+ggplot(dat, aes(x=ppt_trt, y=seeds_per_stem)) +
+  geom_boxplot() +
+  facet_wrap(~background)
+
+## seeds per stem vs. competition background; faceted by precipitation treatment
+ggplot(dat, aes(x=background, y=seeds_per_stem)) +
+  geom_boxplot() +
+  facet_wrap(~ppt_trt)
+
+
+## ANOVAs 
+  ## name the model something unique
+  ## use function aov(response variable ~ predictor, data = your data frame)
+ppt <- aov(seeds_per_stem~ppt_trt, data = dat)
+summary(ppt)
+
+## Tukey's Honest significant difference test; I like to use this when I have a lot of categories
+## you can see the differences between each pair of categories
+TukeyHSD(ppt)
+
+
+## A few potential next steps (and some pseudo-code just for an example): 
+
+  ## we still have a wide range of nutrient treatments in this data set
+  ## this could explain some of the variation we are seeing in seed output
+  ## we might want to filter the data to only include control nutrient treatments (rather than N fertilization or compost)
+  ## tidyverse is really good at filtering
+
+no_nut <- dat %>%
+  filter(nutrient_col_name_here == "C") ## I moved the script to github adn can no longer see the data, 
+  ## but go ahead and fill in the name of the nutrient column here and the specific value for the control plots 
+  ## R will then filter out only control treatments
+
+  ## we could also compare specific species -> also using the filtering function
+  
+  ## if you want to compare mean values between groups, I like to use tidyverse's group_by() and then summarize() functions 
+mean <- dat %>%
+  group_by(background, ppt_trt, any_other_variables_you_want_to_find_the_average_over) %>% 
+  summarize(your_new_column_name = mean(seeds_per_stem))
+## this block of code would take the mean seeds per stem of trifolium in each specific background-ppt treatment combo
+
+
+
