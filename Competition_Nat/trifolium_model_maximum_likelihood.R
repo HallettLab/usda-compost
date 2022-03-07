edbc9ae72ae9a0cd9e3f5d8e7bfd7ef0ede90871
HelloooNatKat <n.kataoka12@gmail.com>
2022-03-07 00:30:45 -0800

    Prep for Presentation

--- Competition_Nat/trifolium_model_maximum_likelihood.R
+++ Competition_Nat/trifolium_model_maximum_likelihood.R
@@ -5,6 +5,9 @@ library(nlstools)
 library(grid)
 library(gridExtra)
 
+install.packages("dplyr")    # alternative installation of the %>%
+library(dplyr) # needs to be run every time you start R and want to use %>%
+
 ## READ IN THE DATA ##
 #set data pathway 
 datpath <- "~/Desktop/USDA-compost/Data/Competition/"  ##NAT TO EDIT 
@@ -51,7 +54,7 @@ trhi_avba <- dat_actual %>%
 
 ## get data ready for modeling
 trhi_avba_m <- trhi_avba %>%
-  filter(background == "AVBA") %>% ## filter for avba background ##I changed AVBA to TACA to see if max_density_halfm2 is the problem...I get an error I think has to do with the fact TACA has 10 rows and AVBA has 9 rows...
+  filter(background = "AVBA") %>% ## filter for avba background ##I changed AVBA to TACA to see if max_density_halfm2 is the problem...I get an error I think has to do with the fact TACA has 10 rows and AVBA has 9 rows...
   
 ###REMOVED: used for the trhi_avba_m table 
 #mutate(TRseedin = as.numeric(tot_stems), ## doing this by the # phytometer stems as seeds in (3 in every case, is not enough info for model)
@@ -70,9 +73,6 @@ trhi_avba_m <- trhi_avba %>%
 TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric 
 
 ###JEFF ADVICE FOR SIMULATION
-#rpois? cbind.data.frame?
-#simualte random normal values rnorm->cbind->then add treatment column
-#rnorm of 100, at the mean and sd of existing data (Tout, Tin, AVout)
 
 #vector (just one column from the original dataframe)
  TRseed_out_new.mean=mean(trhi_avba_m$tot_seeds)
@@ -82,7 +82,6 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  AVstemdens_new.mean=mean(trhi_avba_m$mean_density_halfm2)
  AVstemdens_new.sd=sd(trhi_avba_m$mean_density_halfm2)
  
-##JEFF CODE 
  
 #N1= TRseed_in
 #N2= AVseed_out
@@ -90,8 +89,8 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
   # Set arbitrary parameters
  N<-100 # number of data points you want to simulate
  N1.mean<-2.363636;  N1.std<-0.67419;  N1.max<-100  # Trif stats (intraspecific)
- N2.mean<-13.181818; N2.std<-14.972;  N2.max<-70 #Avena stats (interspecific)
- fitness.mean<-50; fitness.std<-20 #fitness = how many seeds produced? estimate avg. Trif seed produced (per plant)
+ N2.mean<-13.181818; N2.std<-14.972;  N2.max<-100 #Avena stats (interspecific)
+ fitness.mean<-80; fitness.std<-60 #fitness = how many seeds produced? estimate avg. Trif seed produced (per plant)
  
  
  ## Simple random 
@@ -115,29 +114,25 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
    geom_point() +
    geom_smooth(method = "lm", formula = y ~ x + I(x^2))
  
- # These dont reflect having negative density dependence, right?
- 
- # So... 
- # Try simulating fitness actually based on the beverton-holt function
  
  # Set arbitrary parameters
  N<-100 # number of data points
- lam.true<-50 # true lambda (fitness, no competition means exponentially increasing pop.)
+ lambda<-50 # true lambda (fitness, no competition means exponentially increasing pop.)
       ###in Bev-Holt model = growth rate accounting for effects of comp.  
       ###1vs0 = intraspecific comp. promotes coexistence 
- alpha1.true<-.05 # competition coefficient - you could play around with different intra- and inter-sp strengths of competition
+aiT<-.05 # competition coefficient - you could play around with different intra- and inter-sp strengths of competition
  #how was .05 calculated??
- alpha2.true<-.01 # competition coefficient; weaker interaction 
- N1.mean<-30; N1.std<-5; N1.max<-100
- N2.mean<-30; N2.std<-5; N2.max<-70
+aiA<-.01 # competition coefficient; weaker interaction 
+N1.mean<-2.363636;  N1.std<-0.67419; N1.max<-100
+N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
  
- bev1 <- function(N1,N2, lam, alpha1,alpha2) {lam / (1+alpha1*N1+alpha2*N2)} # Beverton-Holt
+ bev1 <- function(N1,N2, lamdba, aiT,aiA) {lambda / (1+aiT*N1+aiA*N2)} # Beverton-Holt
  
  # simulate data
  datXC = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, lam.true, alpha1.true, alpha2.true) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, lambda, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "XC")
  
@@ -145,7 +140,7 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  datW = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, 70, alpha1.true, alpha2.true) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, 70, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "W")
  
@@ -155,24 +150,25 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  datD = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, 20, alpha1.true, alpha2.true) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, 20, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "D")
  
  dat<-bind_rows(datXC,datW,datD)
  
  ggplot(dat, aes(x= N1, y=fitness_data, color=treatment)) +
-   geom_point() 
+   geom_point()+
+ geom_smooth(aes(col=treatment), method="lm", se=F)
  
  ggplot(dat, aes(x= N2, y=fitness_data, color=treatment)) +
-   geom_point() 
+   geom_point()+
+   geom_smooth(aes(col=treatment), method="lm", se=F)
 ###ADD LINES
  dat
  dat$fitness_data[dat$fitness_data<0] <- 0 #lambda values below 0 are just equal to 0
  
 #  ,fitness_data=ifelse(treatment %in% c("W"), fitness_data=fitness_data+5,fitness_data=fitness_data)#W add effect of add to fitness and add error; rnorm randomly
-#   ,fitness_data=ifelse(treatment %in% c("D"), fitness_data=fitness_data-5,fitness_data=fitness_data#D add effect of subtract from fitness and add error; rnorm assign treatments
- ))
+#   ,fitness_data=ifelse(treatment %in% c("D"), fitness_data=fitness_data-5,fitness_data=fitness_data#D add effect of subtract from fitness and add error; rnorm assign treatments))
  
  #example ifelse: mydata$y = ifelse(mydata$x3 %in% c("A","D") ,mydata$x1*2,mydata$x1*3)
  #command Z (undo)
@@ -182,7 +178,7 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
    geom_smooth(method = "lm", formula = y ~ x + I(x^2))+
    stat_smooth(method = "nls",
                formula = y ~ a/(1+b*x), # BH
-               method.args = list(start = list(a = lam.true, b = .1)),
+               method.args = list(start = list(a = lambda, b = .1)),
                se = FALSE, 
                color='orange')
  #randomness about "this" shape (due to Bev-Holt model, as oppossed to just normal.distrib. randomness)
@@ -194,15 +190,13 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
    geom_smooth(method = "lm", formula = y ~ x + I(x^2))+
    stat_smooth(method = "nls",
                formula = y ~ a/(1+b*x), # BH
-               method.args = list(start = list(a = lam.true, b = .1)),
+               method.args = list(start = list(a = lambda, b = .1)),
                se = FALSE, 
                color='orange')
  #graphi curved; fitness of N1 related to pop.size N1
  #graph straight line; fitness N1 is not correlated to pop.size to N2
- #is the goal to replace N1 and N2 with TRHIseedin and TRHIseedout in my model?
  
 # SO, you could try fitting your model to the data in dat
-
 
 ###END SIMULATION 
 
@@ -212,18 +206,15 @@ tg <- 0.2 ## What I think is germination from Andrew's code in the USDA Climvar 
 #eg <- .6 ## erodium germination from Lauren's models_no_facilitation script
 ag <- .9 ## avena germination from Lauren's models_no_facilitation script
 
-
-################################################################################
 ## Model TRHI in AVBA background ##
 
 ## NOTE: we have NOT filtered for nutrient treatment. This script is only parsing by precip treatment
-## I chose to do this as we do not have enough data for TRHI otherwise
 ## we are therefore making the assumption that compost application vs. fertilizer application does NOT make a difference
 
 ## model formula
 m1TinA <- as.formula(log(fitness_data +1) ~  log(tg*(N1+1)*exp(log(lambda)-log((1+aiT*(N1+1)*tg+aiA*(N2+1)*ag))))) 
-## NOTE TO NAT: AVstemdens goes into this model near the end
-
+## AVstemdens goes into this model near the end
+# ai = competition coefficient; effect of that species on the focal phytometer 
 
 ## Run the model in a for loop
 #treatments <- unique(trhi_avba_m$ppt_trt) ## create vector of unique precipitation treatments
@@ -247,9 +238,53 @@ for (i in 1:length(treatments)){
   TRoutput <- rbind(TRoutput, outreport)
 }
 
-##NAT MAYBE PLOT?
-ggplot(dat, aes(x= N1, y=fitness_data, color=treatment)) +
-  geom_point() 
+
+##GROWTH PLOT
+
+annual_growth_competition <- function(N0, lambda, aiT, aiA, aiAA, aiTA, tg, ag, period){
+  # Argument definitions:
+N0 = 26 #starting population size
+aiAA = 0.001 #affect of Avena vs. Avena 
+aiTA = 0.03 #affect of Trifolium vs. Avena 
+period = 50
+  # lambda = per capita growth rate in the absence of competition
+  # alpha = strength of intraspecific competition
+  # g = germination rate (between 0 - 1)
+  
+  t <- c() # create an empty vector to store timesteps in
+  Nt_aiT <- c() # create an empty vector to store pop sizes in
+  Nt_aiAA <- c() # create an empty vector to store pop#2 sizes in
+  fec_aiT <- c() # create an empty vector to store fecundities in
+  fec_aiAA <- c() # create an empty vector to store pop#2 fecundities in
+  
+  Nt_aiT[1] <- N0 # first item in pop size vector is starting pop size
+  Nt_aiAA[1] <- N0 # first item in pop size vector is starting pop size
+  
+  t[1] <- 1 # first item in timestep vector is 1
+  fec_aiT[1] <- lambda/(1 + aiT*N0*tg+ aiA*N0*ag) # calculating the fecundity at time 1
+  fec_aiAA[1] <- lambda/(1 + aiAA*N0*ag+ aiTA*N0*tg) # calculating the fecundity pop#2 at time 1
+  
+  for(i in 2:period){
+    t[i] <- i # store each time step
+    
+    fec_aiT[i] <- lambda/(1 + aiT*Nt_aiT[i-1]*tg+aiA*Nt_aiAA[i-1]*ag) # store each new fecundity
+    fec_aiAA[i] <- lambda/(1 + aiAA*Nt_aiAA[i-1]*ag+aiTA*Nt_aiAA[i-1]*tg)
+    
+    Nt_aiT[i] <- (((1-tg))*Nt_aiT[i-1])+tg*Nt_aiT[i-1]*fec_aiT[i-1] # store each pop size
+    Nt_aiAA[i] <- (((1-ag))*Nt_aiAA[i-1])+ag*Nt_aiAA[i-1]*fec_aiAA[i-1] # store each pop size
+  }
+  dat <- cbind.data.frame(t, Nt_aiT, Nt_aiAA) # bind together time and pop size vectors
+  
+  ggplot(data = dat) + geom_line(aes(x = t, y = Nt_aiT), color = "green") + geom_line(aes(x = t, y = Nt_aiAA), color = "orange") +
+    theme_classic() + xlab("Years") + ylab("Population Size")
+  
+}
+
+
+
+#HALLETT PAPER PLOT CODE
+#ggplot(TRoutput, aes(x=treatment, y=aiA)) + geom_bar(stat="identity") + facet_wrap(~species)
+#ggplot(TRoutput, aes(x=treatment, y=aiT)) + geom_bar(stat="identity") + facet_wrap(~species)
 
 
 ###JEREMY ADVICE FOR CHECKING DATA
@@ -282,54 +317,3 @@ ggplot(trhi_avba_m, aes(x=tot_seed_mass, y=mean_density_halfm2, color = ppt_trt)
 ggplot(trhi_avba_m, aes(x=tot_stem_mass, y=mean_density_halfm2, color = ppt_trt))+
   geom_point()
 
-##Make bar plot (lambda, treatments)
-
-#### It seems to be estimating the growth rate (lambda) for all precipitation conditions but is giving back competition coefficients (alphas) of 0 or 1. Is the model fitting well or are there other tweaks that should be made to it.
-
-
-################################################################################
-## Nat's code ## unedited by Carmen
-
-
-## Competitor species
-
-TRIBACKseedin <- 1250 ## THIS IS NOT NEEDED ANYMORE
-
-# ai = competition coefficient; effect of that species on the focal phytometer 
-
-## Trifolium model - annual plant competition model found in Hallett et al. 2019
-m1T <- as.formula(log(TRseedout +1) ~  log(tg*(TRseedin+1)*exp(log(lambda)-log((1+aiT*(TRseedin+1)*tg+aiTB*(TRIBACKseedin+1)*tg)))))
-## ahh okay the AVseedin part is where we need the background seeds in. 
-## THIS IS WHERE WE ARE STILL MISSING INFORMATION ## 
-
-## JUST CONTROL TEST RUN
-TRIBACKseedin <- 0
-
-m1T <- as.formula(log(TRseedout +1) ~  log(tg*(TRseedin+1)*exp(log(lambda)-log((1+aiT*(TRseedin+1)*tg+aiTB*(TRIBACKseedin+1)*tg)))))
-treatments <- unique(dat_ppt$ppt_trt) ## create a vector of unique treatments (control (XC) and dry (D))
-
-TRoutput <- as.data.frame(matrix(nrow = 0, ncol = 7)) ## make an empty data frame to store the outputs of the for loop below
-names(TRoutput) = c("estimate", "se", "t", "p", "params", "treatment", "species") ## rename columns fo empty dataframe
-
-for (i in 1:length(treatments)){ ## start the for loop; run once for each treatment
-  
-  #eg <- ifelse(treatments[i] == "consistentDry" | treatments[i] == "fallDry", 0.64, .6) ## I think this counts for differential germination in wet and dry...
-  ## taking this out as we do not have this info, we'll just use one germination estimate
-  
-  ## this line below is using maximum likely hood and the model we specified above (m1T) to estimate lambda and alphas
-  m1out <- nlsLM(m1T, start=list(lambda=1, aiT = .01, aiTB=.01), ## aiA will change depending on which competitive background we use
-                 lower = c(0, 0, 0), upper = c(200, 1, 1),
-                 control=nls.lm.control(maxiter=500), trace=T,
-                 data = subset(tr1, !is.na(TRseedout) & ppt_trt == treatments[i]))
-  
-  ## lines below are getting the model outputs into a data frame that matches the format & column names of 
-  ## TRouput, the empty data frame we created above
-  outreport <- as.data.frame(summary(m1out)$coef[1:3, 1:4])
-  names(outreport) = c("estimate", "se", "t", "p")
-  outreport$params <- row.names(outreport)
-  outreport$treatment <- treatments[i] ## make a treatment column in the final data frame
-  outreport$species <- "Trifolium" ## make a column for species in the final data frame
-  TRoutput <- rbind(TRoutput, outreport) ## appending the info from each loop into the empty dataframe
-} ## end the for loop here
-
-## GOAL: different intraspecific comp. in density + precip treatments
