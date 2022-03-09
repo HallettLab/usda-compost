35ab4b2e4ecee5af436e5ba581a8eafe8b8f8f33
HelloooNatKat <n.kataoka12@gmail.com>
2022-03-09 10:03:35 -0800

    clean code

--- Competition_Nat/trifolium_model_maximum_likelihood.R
+++ Competition_Nat/trifolium_model_maximum_likelihood.R
@@ -5,9 +5,6 @@ library(nlstools)
 library(grid)
 library(gridExtra)
 
-install.packages("dplyr")    # alternative installation of the %>%
-library(dplyr) # needs to be run every time you start R and want to use %>%
-
 ## READ IN THE DATA ##
 #set data pathway 
 datpath <- "~/Desktop/USDA-compost/Data/Competition/"  ##NAT TO EDIT 
@@ -21,8 +18,6 @@ datfiles <- dats <- list.files(paste0(datpath, "Competition_CleanedData"), full.
 dat<- read.csv(paste0(datpath, "Competition_CleanedData/competition_trifolium_seeds_2021.csv"), strip.white = T)
 
 
-
-################################################################################
 ## CLEAN UP THE DATA FOR USE IN MODELS ##
 ## keep only the no nutrient treatment (N) and only TRHI (Control) and LOMU (another background species for comparison)
 ## Also filter out wet treatment so we are left with drought and control treatments
@@ -34,8 +29,8 @@ dat_actual <- dat %>%
   filter(tot_stems > 0) ## filters for plants that grew
 
 ## check each background to see how many rows there are for each.
-trhi_avba <- dat_actual %>%
-  filter(background == "AVBA") ##changed AVBA to TACA to test NaN issues 
+trhi_avba_m <- dat_actual %>%
+  filter(background == "AVBA") 
 ## AVBA has the most rows, so it might be best to use this for the purposes of your project. 
 ## Otherwise I think we will be struggling to fit models with not enough data
 
@@ -53,8 +48,8 @@ trhi_avba <- dat_actual %>%
 
 
 ## get data ready for modeling
-trhi_avba_m <- trhi_avba %>%
-  filter(background = "AVBA") %>% ## filter for avba background ##I changed AVBA to TACA to see if max_density_halfm2 is the problem...I get an error I think has to do with the fact TACA has 10 rows and AVBA has 9 rows...
+#trhi_avba_m <- trhi_avba %>%
+  #filter(background == "AVBA") %>% ## filter for avba background 
   
 ###REMOVED: used for the trhi_avba_m table 
 #mutate(TRseedin = as.numeric(tot_stems), ## doing this by the # phytometer stems as seeds in (3 in every case, is not enough info for model)
@@ -82,10 +77,6 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  AVstemdens_new.mean=mean(trhi_avba_m$mean_density_halfm2)
  AVstemdens_new.sd=sd(trhi_avba_m$mean_density_halfm2)
  
- 
-#N1= TRseed_in
-#N2= AVseed_out
- 
   # Set arbitrary parameters
  N<-100 # number of data points you want to simulate
  N1.mean<-2.363636;  N1.std<-0.67419;  N1.max<-100  # Trif stats (intraspecific)
@@ -97,7 +88,6 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  dat.simple = tibble(
    N1 = round(runif(N, 0,N1.max)) # seems to me it makes more sense to have a full density gradient rather than just data surrounding the mean.
    ,N2 = round(runif(N, 0,N2.max)) 
-   #,N2 = round(rnorm(N, N2.mean,N2.std))# replace 30 with mean and 5 with std
    ,fitness_data = round(rnorm(N, fitness.mean, fitness.std))
    ,treatment = sample(c("XC","W","D"), size=N, replace=T) # randomly assign treatments
  ) #checking data...shows no correlation
@@ -120,19 +110,18 @@ TRseed_in_new = as.numeric(trhi_avba_m$tot_stems) ##numeric
  lambda<-50 # true lambda (fitness, no competition means exponentially increasing pop.)
       ###in Bev-Holt model = growth rate accounting for effects of comp.  
       ###1vs0 = intraspecific comp. promotes coexistence 
-aiT<-.05 # competition coefficient - you could play around with different intra- and inter-sp strengths of competition
- #how was .05 calculated??
+aiT<-.1 # competition coefficient 
 aiA<-.01 # competition coefficient; weaker interaction 
 N1.mean<-2.363636;  N1.std<-0.67419; N1.max<-100
 N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
  
- bev1 <- function(N1,N2, lamdba, aiT,aiA) {lambda / (1+aiT*N1+aiA*N2)} # Beverton-Holt
+ bev1 <- function(N1,N2, lambda, aiT,aiA) {lambda / (1+aiT*N1+aiA*N2)} # Beverton-Holt
  
  # simulate data
  datXC = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, lambda, aiT, aiA) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, 50, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "XC")
  
@@ -140,7 +129,7 @@ N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
  datW = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, 70, aiT, aiA) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, 75, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "W")
  
@@ -150,7 +139,7 @@ N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
  datD = tibble(
    N1 = round(runif(N, 0,N1.max))
    ,N2 = round(runif(N, 0,N2.max))
-   ,fitness_sim = bev1(N1,N2, 20, aiT, aiA) # fitness, using BH
+   ,fitness_sim = bev1(N1,N2, 15, aiT, aiA) # fitness, using BH
    ,fitness_data = fitness_sim  + rnorm(N,0, 5) # add some noise; adding random amount of error
    ,treatment = "D")
  
@@ -172,6 +161,7 @@ N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
  
  #example ifelse: mydata$y = ifelse(mydata$x3 %in% c("A","D") ,mydata$x1*2,mydata$x1*3)
  #command Z (undo)
+ 
  # plot relative to N1
  ggplot(dat, aes(x= N1, y=fitness_data)) +
    geom_point() +
@@ -193,7 +183,7 @@ N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
                method.args = list(start = list(a = lambda, b = .1)),
                se = FALSE, 
                color='orange')
- #graphi curved; fitness of N1 related to pop.size N1
+ #graph curved; fitness of N1 related to pop.size N1
  #graph straight line; fitness N1 is not correlated to pop.size to N2
  
 # SO, you could try fitting your model to the data in dat
@@ -202,7 +192,7 @@ N2.mean<-13.181818; N2.std<-14.972; N2.max<-100
 
 ## germination and survival fractions
 ## Trifolium
-tg <- 0.2 ## What I think is germination from Andrew's code in the USDA Climvar repository; DOUBLE CHECK this is correct
+tg <- 0.2 ## germination from Andrew's code in the USDA Climvar repository
 #eg <- .6 ## erodium germination from Lauren's models_no_facilitation script
 ag <- .9 ## avena germination from Lauren's models_no_facilitation script
 
@@ -212,41 +202,39 @@ ag <- .9 ## avena germination from Lauren's models_no_facilitation script
 ## we are therefore making the assumption that compost application vs. fertilizer application does NOT make a difference
 
 ## model formula
-m1TinA <- as.formula(log(fitness_data +1) ~  log(tg*(N1+1)*exp(log(lambda)-log((1+aiT*(N1+1)*tg+aiA*(N2+1)*ag))))) 
+#m1TinA <- as.formula(log(fitness_data +1) ~  log(tg*(N1+1)*exp(log(lambda)-log((1+aiT*(N1+1)*tg+aiA*(N2+1)*ag))))) 
 ## AVstemdens goes into this model near the end
 # ai = competition coefficient; effect of that species on the focal phytometer 
 
 ## Run the model in a for loop
 #treatments <- unique(trhi_avba_m$ppt_trt) ## create vector of unique precipitation treatments
 
-TRoutput <- as.data.frame(matrix(nrow = 0, ncol = 7)) ## create empty data frame to contain output of for-loop
-names(TRoutput) = c("estimate", "se", "t", "p", "params", "treatment", "species") ## rename those columns
+#TRoutput <- as.data.frame(matrix(nrow = 0, ncol = 7)) ## create empty data frame to contain output of for-loop
+#names(TRoutput) = c("estimate", "se", "t", "p", "params", "treatment", "species") ## rename those columns
 
 
-for (i in 1:length(treatments)){
-  #eg <- ifelse(treatments[i] == "consistentDry" | treatments[i] == "fallDry", 0.64, .6)
-  m1out <- nlsLM(m1TinA, start=list(lambda=1, aiT = .01, aiA=.01),
-                 lower = c(0, 0, 0), upper = c(200, 1, 1),
-                 control=nls.lm.control(maxiter=500), trace=T,
-                 data = subset(dat, treatment == treatments[i]))
+#for (i in 1:length(treatments)){
+ 
+  #m1out <- nlsLM(m1TinA, start=list(lambda=50, aiT = .1, aiA=.01),
+                 #lower = c(0, 0, 0), upper = c(200, 1, 1),
+                 #control=nls.lm.control(maxiter=500), trace=T,
+                 #data = subset(dat, treatment == treatments[i]))
   
-  outreport <- as.data.frame(summary(m1out)$coef[1:3, 1:4])
-  names(outreport) = c("estimate", "se", "t", "p")
-  outreport$params <- row.names(outreport)
-  outreport$treatment <- treatments[i]
-  outreport$species <- "Trifolium"
-  TRoutput <- rbind(TRoutput, outreport)
-}
+  #outreport <- as.data.frame(summary(m1out)$coef[1:3, 1:4])
+  #names(outreport) = c("estimate", "se", "t", "p")
+  #outreport$params <- row.names(outreport)
+  #outreport$treatment <- treatments[i]
+  #outreport$species <- "Trifolium"
+  #TRoutput <- rbind(TRoutput, outreport)
+#}
 
+##GROWTH PLOT 
 
-##GROWTH PLOT
-
-annual_growth_competition <- function(N0, lambda, aiT, aiA, aiAA, aiTA, tg, ag, period){
+dat2<-annual_growth_competition(N0=300, lambda=50, aiT=0.1, aiA=0.01, aiAA=0.02, aiTA=0.04, tg=0.2, ag=0.9, period=50){
   # Argument definitions:
-N0 = 26 #starting population size
-aiAA = 0.001 #affect of Avena vs. Avena 
-aiTA = 0.03 #affect of Trifolium vs. Avena 
-period = 50
+#N0 = 26 #starting population size
+#aiAA = 0.001 #affect of Avena vs. Avena 
+#aiTA = 0.03 #affect of Trifolium vs. Avena 
   # lambda = per capita growth rate in the absence of competition
   # alpha = strength of intraspecific competition
   # g = germination rate (between 0 - 1)
@@ -273,18 +261,10 @@ period = 50
     Nt_aiT[i] <- (((1-tg))*Nt_aiT[i-1])+tg*Nt_aiT[i-1]*fec_aiT[i-1] # store each pop size
     Nt_aiAA[i] <- (((1-ag))*Nt_aiAA[i-1])+ag*Nt_aiAA[i-1]*fec_aiAA[i-1] # store each pop size
   }
-  dat <- cbind.data.frame(t, Nt_aiT, Nt_aiAA) # bind together time and pop size vectors
+  dat2 <- cbind.data.frame(t, Nt_aiT, Nt_aiAA)} # bind together time and pop size vectors
   
-  ggplot(data = dat) + geom_line(aes(x = t, y = Nt_aiT), color = "green") + geom_line(aes(x = t, y = Nt_aiAA), color = "orange") +
-    theme_classic() + xlab("Years") + ylab("Population Size")
-  
-}
-
-
-
-#HALLETT PAPER PLOT CODE
-#ggplot(TRoutput, aes(x=treatment, y=aiA)) + geom_bar(stat="identity") + facet_wrap(~species)
-#ggplot(TRoutput, aes(x=treatment, y=aiT)) + geom_bar(stat="identity") + facet_wrap(~species)
+ggplot(data = dat2) + geom_line(aes(x = t, y = Nt_aiT), color = "green") + geom_line(aes(x = t, y = Nt_aiAA), color = "orange") +
+  theme_classic() + xlab("Years") + ylab("Population Size") 
 
 
 ###JEREMY ADVICE FOR CHECKING DATA
